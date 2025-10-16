8348a1837bffdc3e01854c9836a86f910aa96f8c
ByteByAbhay <prajapatiabhay.p22@gmail.com>
2025-10-16 07:44:35 +0530

    chore: add test log in route registration

--- src/app.js
+++ src/app.js
@@ -155,6 +155,7 @@ const routes = [
 const apiRouter = express.Router();
 routes.forEach(({ path, router, name }) => {
   apiRouter.use(path, router);
+  console.log("test - cicd")
   console.log(`✓ Registered route: /api${name ? `/${name}` : path}`);
 });
 
